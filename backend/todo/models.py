beb89b65af02818507511d990ea1f3ab920bf68a
naveennvrgup <naveennvrgup@gmail.com>
2019-04-03 13:45:43 +0530

    models nearly ready

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,6 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
 
 
-class Todo:
-    user = models.ForeignKey
+class Todo(models.Model):
+    user = models.ForeignKey(User, related_name='todos',
+                             on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateTimeField(auto_now=True)
+    task = models.CharField(max_length=500)
+    description = models.TextField(null=True)
+    start = models.DateTimeField(null=True)
+    end = models.DateTimeField(null=True)
+    notified = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
+    version = models.IntegerField(default=0)
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class TodoSerializer(ModelSerializer):
+    class Meta:
+        fields = '__all__'
+        model = Todo
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from rest_framework.decorators import permission_classes, api_view
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
+from .models import *
 
-# Create your views here.
+
+@api_view(['post'])
+@permission_classes([AllowAny])
+def signup_view(req):
+    data = JSONParser().parse(req)
+    try:
+        user = User.objects.create_user(
+            username=data['username'],
+            password=data['password'],
+            email=data['email']
+        )
+        sdata = UserSerializer(User.objects.all()[0])
+        print(sdata.data)
+        return Response({
+            **sdata.data,
+            "password": ''
+        })
+    except:
+        return Response({'error': 'something went wrong'})
+
+
+@api_view(['get'])
+def list_view(req):
+    sdata = TodoSerializer(Todo.objects.all(), many=True)
+    return Response(sdata.data)
+
+
+@api_view(['get', 'post', 'delete'])
+def show_view(req, id):
+    if req.method == 'DELETE':
+        todo = Todo.objects.get(pk=id)
+        sdata = TodoSerializer(todo)
+        todo.delete()
+        return Response(sdata.data)
+    elif req.method == 'GET':
+        sdata = TodoSerializer(Todo.objects.get(pk=id))
+        return Response(sdata.data)
+
+    # post
+    data = JSONParser().parse(req)
+    todo = Todo.objects.create(**data)
+    sdata = TodoSerializer(todo)
+    return Response(sdata.data)
